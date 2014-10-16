51353e7a214db964994e327be7c4363d2d4df838
Tobias Löb <loeb@bitformer.net>
2014-10-16 15:23:17 +0200

    ADDED Twitter Bootstrap Framework Plugin 3.2.0.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -72,5 +72,8 @@ grails.project.dependency.resolution = {
         //compile ":less-asset-pipeline:1.10.0"
         //compile ":coffee-asset-pipeline:1.8.0"
         //compile ":handlebars-asset-pipeline:1.3.0.3"
+		
+		//Bootstrap Plugin
+		runtime ':twitter-bootstrap:3.2.0.2'
     }
 }
