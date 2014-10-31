b3823e30842e78c1c2cb022ce9a2567217099e88
Tobias Löb <loeb@bitformer.net>
2014-10-31 22:19:38 +0100

    ADDED: Grails-Security Plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -61,11 +61,14 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.7'
         compile ":asset-pipeline:1.9.6"
+		compile ':spring-security-core:2.0-RC4'
+		compile ":spring-security-ui:1.0-RC2"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
+				
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
